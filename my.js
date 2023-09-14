a58224aca533bc2271b58a639706f95da76fae7f
Khanhpear97 <khanhpear97@gmail.com>
2023-09-14 18:20:10 +0700

    first commit

---
+++ my.js
@@ -0,0 +1,19 @@
+function check() {
+    let month = +document.getElementById('month').value;
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            document.getElementById('result').innerHTML = 'Tháng ' + month + ' có 31 ngày';
+            break;
+        case 2:
+            document.getElementById('result').innerHTML = 'Tháng ' + month + ' có 28 hoặc 29 ngày';
+            break;
+        default:
+            document.getElementById('result').innerHTML = 'Tháng ' + month + ' có 30 ngày';
+    }
+}
